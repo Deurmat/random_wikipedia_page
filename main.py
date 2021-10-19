5d536a89f21b663d1ff4dd3ab6636133ed7581d0
JP <janpieterb@gmail.com>
2021-10-19 13:02:19 +0200

    Added random query First random url based on ID

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import requests
 
+s = requests.Session()
 
+url = "https://en.wikipedia.org/w/api.php"
+params = {
+    "action": "query",
+    "format": "json",
+    "list": "random",
+    "rnlimit": "1"
+}
 
+r = s.get(url=url, params=params)
+j = r.json()
+#print(j['query']['random'])
+
+base_url = f"https://en.wikipedia.org/?curid={j['query']['random'][0]['id']}"
+print(f'Your URL is {base_url}')
