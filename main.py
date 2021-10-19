31c40c0abb7361374c0dd0de0d954a9ae6e5a583
JP <janpieterb@gmail.com>
2021-10-19 19:31:12 +0200

    Added generator - wip

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ url = "https://en.wikipedia.org/w/api.php"
 params = {
     "action": "query",
     "format": "json",
-    "list": "random",
-    "rnlimit": "1"
+    # "list": "random",
+    "generator": "random",
+    "rnlimit": "1",
+    "rnnamespace": 0,
+    "prop": "links"
 }
 
 r = s.get(url=url, params=params)
 j = r.json()
 #print(j['query']['random'])
-
+print(j)
 base_url = f"https://en.wikipedia.org/?curid={j['query']['random'][0]['id']}"
 print(f'Your URL is {base_url}')
+
+# http://it.wikipedia.org/w/api.php?action=query&list=search&srsearch=calvino&format=xml&srprop=snippet
+
+# http://it.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=calvino&format=xml&gsrprop=snippet&prop=info&inprop=url
